d22b2da59b2b8c2baab87249a6107df4d0ea98e2
gideon.c <gideon.c@ITPL-SOFT-001>
2020-03-07 15:50:15 +0530

    Added CURDAjax

---
+++ WebAjaxSampleApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAjaxSampleApplication.MvcApplication" Language="C#" %>
---
+++ WebAjaxSampleApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAjaxSampleApplication.Startup))]
+namespace WebAjaxSampleApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
